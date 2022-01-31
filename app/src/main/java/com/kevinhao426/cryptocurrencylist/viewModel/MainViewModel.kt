f7bb46a075ed60cbf6f8a1081383214ad0535997
kehao <kevinhao426@gmail.com>
2022-01-31 16:07:14 +1000

    add currency data read logic

--- app/src/main/java/com/kevinhao426/cryptocurrencylist/viewModel/MainViewModel.kt
+++ app/src/main/java/com/kevinhao426/cryptocurrencylist/viewModel/MainViewModel.kt
@@ -1,10 +1,29 @@
 package com.kevinhao426.cryptocurrencylist.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kevinhao426.cryptocurrencylist.dao.AppDatabase
+import com.kevinhao426.cryptocurrencylist.model.CurrencyData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
+
+    val currencyListLiveData = MutableLiveData<List<CurrencyData>>()
+
+    fun fetchCurrencyList(appDatabase: AppDatabase) {
+        viewModelScope.launch {
+            appDatabase.currencyDataDao().getAll().let {
+                if (it.isNullOrEmpty()) {
+                    //todo: io to inject data from json string to db
+                } else {
+                    currencyListLiveData.postValue(it)
+                }
+            }
+        }
+
+    }
 }
